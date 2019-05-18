38592211fb123fbb802f9e95a2bc6afed774a895
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-05-18 16:43:38 +0900

    twitter apiからデータを取得

--- server/lambda/index.js
+++ server/lambda/index.js
@@ -1,9 +1,16 @@
+const Twitter = require("twitter-promise")
+const twitter = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_BOT_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_BOT_ACCESS_TOKEN_SECRET,
+});
+
 exports.handler = async (event, context) => {
-    console.log(event);
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return event;
+  console.log(event);
+  const result = await twitter.get({
+    path: "search/tweets",
+    params: Object.assign({ count: 100 }, event)
+  })
+  return result;
 };
