1a6d7282943375fb437bbaf57e27dd7890c57cc7
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-05-19 11:30:23 +0900

    移動させたりモック画面をおラット入れたり

---
+++ Spajam2019/Assets/Scripts/MainUIController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainUIController : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> uipages;
+
+    private int currentPage = 0;
+
+    void Start()
+    {
+        ShowCurrentPage();
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    public void OnMovePageButtonClicked(int page)
+    {
+        currentPage = currentPage + page;
+        currentPage = Mathf.Max(currentPage, 0);
+        currentPage = Mathf.Min(currentPage, uipages.Count);
+        ShowCurrentPage();
+    }
+
+    private void ShowCurrentPage()
+    {
+        for(int i = 0;i < uipages.Count; ++i)
+        {
+            uipages[i].gameObject.SetActive(i == currentPage);
+        }
+    }
+}
