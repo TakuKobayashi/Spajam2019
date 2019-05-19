2ab14b2c7e661003c7ac60421a2ae08335b8f3ef
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-05-19 10:57:37 +0900

    VRMをなんかいい感じの位置に表示するようにした

---
+++ Spajam2019/Assets/Scripts/NavigaterController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavigaterController : MonoBehaviour
+{
+    [SerializeField] GameObject navigaterCharacter;
+    [SerializeField] Vector3 characterPosition;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform cameraTransform = Camera.main.transform;
+        navigaterCharacter.transform.position = cameraTransform.position - characterPosition;
+        
+        navigaterCharacter.transform.LookAt(new Vector3(cameraTransform.position.x, navigaterCharacter.transform.position.y, cameraTransform.position.z));
+    }
+}
